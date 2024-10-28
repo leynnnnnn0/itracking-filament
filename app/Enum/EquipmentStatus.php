12b5c352be592b7456918547e2efaa906e70f21d
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-10-28 20:20:24 +0800

    added tables for equipment related models

--- app/Enum/EquipmentStatus.php
+++ app/Enum/EquipmentStatus.php
@@ -2,10 +2,16 @@
 
 namespace App\Enum;
 
-enum EquipmentStatus : string
+enum EquipmentStatus: string
 {
     case ACTIVE = 'active';
     case PARTIALLY_BORROWED = 'partially_borrowed';
     case FULLY_BORROWED = 'fully_borrowed';
     case CONDEMNED = 'condemned';
+
+    public static function values()
+    {
+        $data = array_column(self::cases(), 'value');
+        return array_combine($data, $data);
+    }
 }
