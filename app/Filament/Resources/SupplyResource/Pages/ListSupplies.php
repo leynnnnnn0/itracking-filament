fcf565c329e513021d4506cfaaaa44b111226d65
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-11-01 10:36:15 +0800

    added pdf download for supplies

--- app/Filament/Resources/SupplyResource/Pages/ListSupplies.php
+++ app/Filament/Resources/SupplyResource/Pages/ListSupplies.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\SupplyResource\Pages;
 
 use App\Filament\Resources\SupplyResource;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Support\Facades\Blade;
 
 class ListSupplies extends ListRecords
 {
@@ -13,7 +15,29 @@ class ListSupplies extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            Actions\Action::make('export_as_pdf')
+                ->color('gray')
+                ->label('Export as PDF')
+                ->icon('heroicon-o-document-arrow-down')
+                ->action(fn() => $this->export()),
             Actions\CreateAction::make(),
         ];
+    }
+
+    public function export()
+    {
+        $query = $this->getFilteredTableQuery();
+        $this->applySortingToTableQuery($query);
+        $supplies = $query->get();
+        return response()->streamDownload(
+            function () use ($supplies) {
+                echo Pdf::loadHtml(
+                    Blade::render('pdf.supplies-list', ['supplies' => $supplies])
+                )
+                    ->setPaper('a3', 'landscape')
+                    ->stream();
+            },
+            'supplies-list.pdf'
+        );
     }
 }
---
+++ resources/views/pdf/supplies-list.blade.php
@@ -0,0 +1,38 @@
+<x-layouts.pdf>
+    <div class="table-header">
+        <h1>{{ "List of Supplies" }}</h1>
+        <p>Generated on: {{ date('F d, Y') }}</p>
+    </div>
+    <table class="print-table">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Description</th>
+                <th>Categories</th>
+                <th>Quantity</th>
+                <th>Used</th>
+                <th>Recently Added</th>
+                <th>Total</th>
+                <th>Expiry Date</th>
+                <th>Is Consumable?</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($supplies as $supply)
+            <tr>
+                <td>{{ $supply->id }}</td>
+                <td>{{ $supply->description }}</td>
+                <td>{{ implode(' / ', $supply->categories->map(function ($item) {
+                            return $item->name;
+                    })->toArray())}}</td>
+                <td>{{ $supply->quantity }}</td>
+                <td>{{ $supply->used }}</td>
+                <td>{{ $supply->recently_added }}</td>
+                <td>{{ $supply->total }}</td>
+                <td>{{ $supply->expiry_date?->format('F d, Y') ?? 'N/a' }}</td>
+                <td>{{ $supply->is_consumable ? 'Yes' : 'No' }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</x-layouts.pdf>
