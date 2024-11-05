f3b974664de138dc0da9fcff1f0c293d9973e835
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-11-06 06:06:35 +0800

    added date filter on borrowed equipmetns

--- app/Filament/Resources/BorrowedEquipmentResource.php
+++ app/Filament/Resources/BorrowedEquipmentResource.php
@@ -193,6 +193,23 @@ class BorrowedEquipmentResource extends Resource
                         $equipment = Equipment::find($value);
                         return "$equipment->name (PN: $equipment->property_number)";
                     }),
+
+                Filter::make('created_at')
+                    ->form([
+                        DatePicker::make('created_from'),
+                        DatePicker::make('created_until'),
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query
+                            ->when(
+                                $data['created_from'],
+                                fn(Builder $query, $date): Builder => $query->whereDate('created_at', '>=', $date),
+                            )
+                            ->when(
+                                $data['created_until'],
+                                fn(Builder $query, $date): Builder => $query->whereDate('created_at', '<=', $date),
+                            );
+                    }),
             ])
             ->actions([
                 Tables\Actions\ActionGroup::make([
